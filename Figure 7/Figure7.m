5a9d33e162cd585f22e37a1d6c002b65e4aa035c
IWaleedHmoud <88354731+IWaleedHmoud@users.noreply.github.com>
2025-08-14 11:42:58 -0400

    Add files via upload

---
+++ Figure 7/Figure7.m
@@ -0,0 +1,58 @@
+clc; clear; close all
+
+T = readtable('C:\Users\iwalhmoud\OneDrive - North Carolina A&T State University\Issa2Gokaraju\Students\Hiba Darwish\RE paper - Jordan case study\Initial draft\Results.xlsx') ;
+
+%% Wind project names
+X1 = categorical(T.ProjectName(1:9));
+X1 = reordercats(X1, T.ProjectName(1:9));
+
+%% Wind Projects --> 3x3
+FS = 12 ; % Font Size
+
+figure;
+t1 = tiledlayout(3, 3) ;
+
+for i = 1:size(X1, 1)
+    nexttile
+    X = categorical(T.ProjectName(i)) ;
+
+    bar(X, [T.BaseCase(i) T.ProposedCase(i) T.Reduction(i)]) ;
+    
+    ax = gca ;
+    ax.FontSize = FS ;
+    ax.FontWeight = "bold" ;
+    fontname("Times New Roman") ;
+    
+    grid on
+end
+
+ylabel(t1, 'Emitted CO_{2} [tCO_{2}]', 'FontSize', 12, 'FontWeight', 'bold');
+
+%% Top PV CO2 emission reduction projects
+figure ;
+t = tiledlayout(3, 3) ;
+TopPV = [13, 17, 20, 21, 22, 23, 24, 26, 27] ;
+PVlge = [13, 22, 27] ; % projects with higher ylimit
+
+for i = 1:size(TopPV, 2)
+    nexttile
+    X = categorical(T.ProjectName(TopPV(i))) ;
+
+    bar(X, [T.BaseCase(TopPV(i)) T.ProposedCase(TopPV(i)) T.Reduction(TopPV(i))]) ;
+    
+    if ismember(PVlge, TopPV)
+        ylim([0 T.BaseCase(TopPV(i)) + 5500]) ;
+    else
+        ylim([0 T.BaseCase(TopPV(i)) + 1500]) ;
+    end
+    
+
+    ax = gca ;
+    ax.FontSize = FS ;
+    ax.FontWeight = "bold" ;
+    fontname("Times New Roman") ;
+    grid on
+
+end
+
+ylabel(t, 'Emitted CO_{2} [tCO_{2}]', 'FontSize', 12, 'FontWeight', 'bold');
---
+++ Figure 8/Figure8.m
@@ -0,0 +1,37 @@
+
+
+Data1 = readtable('C:\Users\iwalhmoud\OneDrive - North Carolina A&T State University\Issa2Gokaraju\Students\Hiba Darwish\RE paper - Jordan case study\Model 1.xlsx') ;
+Data2 = readtable('C:\Users\iwalhmoud\OneDrive - North Carolina A&T State University\Issa2Gokaraju\Students\Hiba Darwish\RE paper - Jordan case study\Model 2.xlsx') ;
+
+
+
+mdl_1 = fitlm(Data1, 'linear') ;
+mdl_2 = fitlm(Data2, 'linear') ;
+
+% New x values for smooth curve
+xq1 = linspace(min(Data1.(1)), max(Data1.(1)), 100)';
+[y_pred1, y_ci1] = predict(mdl_1, xq1, 'prediction', 'observation', 'Alpha', 0.05) ;
+
+xq2 = linspace(min(Data2.(1)), max(Data2.(1)), 100)';
+[y_pred2, y_ci2] = predict(mdl_2, xq2, 'prediction', 'observation', 'Alpha', 0.05) ;
+
+set(groot, 'DefaultAxesFontSize', 12);
+
+%% Model 1 Confidence Interval
+figure; hold on;
+scatter(Data1.(1), Data1.(2), 'bx', 'LineWidth', 2, 'DisplayName', 'Data');
+plot(xq1, y_pred1, 'g', 'LineWidth', 2, 'DisplayName','Prediction') ;
+plot(xq1, y_ci1, 'r--', 'LineWidth', 2, 'DisplayName', '95% Confidence Interval') ;
+grid on; axis tight ; legend('Location','best') ;
+xlabel('Emitted CO_{2} [MtCO_2]', 'Interpreter', 'tex', 'FontSize', 12, 'FontWeight', 'bold');
+ylabel('Trapped Solar Radiation [W/m^2]', 'Interpreter', 'tex', 'FontSize', 12, 'FontWeight', 'bold') ;
+
+
+%% Model 2 Confidence Interval
+figure; hold on;
+scatter(Data2.(1), Data2.(2), 'bx', 'LineWidth', 2, 'DisplayName', 'Data');
+plot(xq2, y_pred2, 'g', 'LineWidth', 2, 'DisplayName','Prediction') ;
+plot(xq2, y_ci2, 'r--', 'LineWidth', 2, 'DisplayName', '95% Confidence Interval') ;
+grid on ; axis tight ; legend('Location', 'best') ;
+xlabel('Trapped Solar Radiation [W/m^2]', 'FontSize', 12, 'FontWeight', 'bold') ;
+ylabel('Average Temperature [°C]', 'Interpreter', 'tex', 'FontSize', 12, 'FontWeight', 'bold') ;
